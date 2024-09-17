05ce559142ac60e10afd206c5e971c38660eaa38
tjrelyts <tylerj-santo@outlook.com>
2024-09-17 11:40:22 -0400

    Rewrite of Git Repo

---
+++ core/src/main/java/com/tyler/m31/Main.java
@@ -0,0 +1,15 @@
+package com.tyler.m31;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+public class Main extends ApplicationAdapter {
+
+    @Override
+    public void create() {}
+
+    @Override
+    public void render() {}
+
+    @Override
+    public void dispose() {}
+}
